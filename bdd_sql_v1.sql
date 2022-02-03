6f07f8c6421dd8f63412779de574e09d186aa5a3
Sokchy <94165662+Sokchy@users.noreply.github.com>
2022-02-03 09:15:49 +0100

    Update bdd_sql_v1.sql

--- bdd_sql_v1.sql
+++ bdd_sql_v1.sql
@@ -6,7 +6,7 @@ DROP TABLE IF EXISTS chaussure;
 DROP TABLE IF EXISTS user;
 
 CREATE TABLE IF NOT EXISTS user (
-    id_user INT AUTO_INCREMENT
+    id INT AUTO_INCREMENT
     ,username VARCHAR(25)
     ,password VARCHAR(100)
     ,role VARCHAR(25)
@@ -16,13 +16,12 @@ CREATE TABLE IF NOT EXISTS user (
     ,PRIMARY KEY (id_user)
 );
 
-INSERT INTO user (id_user, email, username, password, role,  est_actif) VALUES
+INSERT INTO user (id, email, username, password, role,  est_actif) VALUES
 (NULL, 'admin@admin.fr', 'admin', 'sha256$pBGlZy6UukyHBFDH$2f089c1d26f2741b68c9218a68bfe2e25dbb069c27868a027dad03bcb3d7f69a', 'ROLE_admin', 1);
-INSERT INTO user  (id_user, email, username, password, role, est_actif) VALUES
+INSERT INTO user  (id, email, username, password, role, est_actif) VALUES
 (NULL, 'client@client.fr', 'client', 'sha256$Q1HFT4TKRqnMhlTj$cf3c84ea646430c98d4877769c7c5d2cce1edd10c7eccd2c1f9d6114b74b81c4', 'ROLE_client', 1);
-INSERT INTO user  (id_user, email, username, password, role,  est_actif) VALUES
+INSERT INTO user  (id, email, username, password, role,  est_actif) VALUES
 (NULL, 'client2@client2.fr', 'client2', 'sha256$ayiON3nJITfetaS8$0e039802d6fac2222e264f5a1e2b94b347501d040d71cfa4264cad6067cf5cf3', 'ROLE_client',1);
-SELECT * FROM user;
 
 CREATE TABLE IF NOT EXISTS chaussure (
     id_chaussure INT AUTO_INCREMENT
